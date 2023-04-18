916bc6c672c6518fec4d91a6f26fbe69ecc7133c
Alexander Fredheim <alfr@netlight.com>
2023-04-18 14:12:27 +0200

    code readability improvements

--- src/test/java/minesweeper/MinesweeperTest.java
+++ src/test/java/minesweeper/MinesweeperTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MinesweeperTest {
-    private Minesweeper minesweeper = new Minesweeper();
+    private final Minesweeper minesweeper = new Minesweeper();
 
-    String[][] oneByOneBomb = new String[][] {{"*"}};
-    String[][] oneByOneNoBomb = new String[][] {{"-"}};
-    String[][] oneByOneZero = new String[][] {{"0"}};
+    String[][] oneByOneMatrixBomb = new String[][] {{"*"}};
+    String[][] oneByOneMatrixNoBomb = new String[][] {{"-"}};
+    String[][] oneByOneMatrixZero = new String[][] {{"0"}};
 
     String[][] threeByThreeMatrix = new String[][] {
             {"-", "-", "*"},
@@ -19,7 +19,7 @@ class MinesweeperTest {
 
     @Test
     void whenOneByOneMatrixWithBomb_expectGeneratorToReturnMatrixWithBomb(){
-        String[][] generatedBoard = minesweeper.generateMinesweeperBoard(oneByOneBomb);
-        assertEquals(generatedBoard, oneByOneBomb);
+        String[][] generatedBoard = minesweeper.generateMinesweeperBoard(oneByOneMatrixBomb);
+        assertEquals(generatedBoard, oneByOneMatrixBomb);
     }
 }

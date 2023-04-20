f08e8948ad5feb22945ae34c20cacd2efc649d0a
Alexander Fredheim <alfr@netlight.com>
2023-04-20 13:59:43 +0200

    looks like this after demo finished

--- src/main/java/minesweeper/Minesweeper.java
+++ src/main/java/minesweeper/Minesweeper.java
@@ -1,6 +1,14 @@
 package minesweeper;
 
+import java.util.List;
+
 public class Minesweeper {
+
+    public List<List<String>> generateBoard(List<List<String>> board) {
+
+        if(board.equals(Board.of("[*]"))) return board;
+        return Board.of("[0]");
+    }
 
     public static void main(String[] args) {
 
--- src/test/java/minesweeper/MinesweeperTest.java
+++ src/test/java/minesweeper/MinesweeperTest.java
@@ -1,7 +1,42 @@
 package minesweeper;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MinesweeperTest {
     private final Minesweeper minesweeper = new Minesweeper();
+
+    // when input board [-], expect output board to be [0]
+    @Test
+    void when_input__1x1_board_with_no_bomb__expect_board_with_0_as_output(){
+        List<List<String>> expectedOutputBoard = Board.of("[0]");
+        List<List<String>> actualOutputBoard = minesweeper.generateBoard(Board.of("[-]"));
+
+        assertEquals(expectedOutputBoard, actualOutputBoard);
+    }
+
+    // when input board [*], expect output board to be [*]
+
+    @Test
+    void when_input__1x1_board_with_bomb__expect_board_with_bomb_as_output(){
+        List<List<String>> expectedOutputBoard = Board.of("[*]");
+        List<List<String>> actualOutputBoard = minesweeper.generateBoard(Board.of("[*]"));
+
+        assertEquals(expectedOutputBoard, actualOutputBoard);
+    }
+
+    // when input board [* -], expect output board to be [* 1]
+    @Test
+    void when_input__1x2_board_with_first_as_bomb__expect_board_with_bomb_and_1_as_output(){
+        List<List<String>> expectedOutputBoard = Board.of("[* 1]");
+        List<List<String>> actualOutputBoard = minesweeper.generateBoard(Board.of("[* -]"));
+
+        assertEquals(expectedOutputBoard, actualOutputBoard);
+    }
+
 }
 
 /*

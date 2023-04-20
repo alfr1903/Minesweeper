d15dc994b3d224b33e3f53090f515aff9507a868
Alexander Fredheim <alfr@netlight.com>
2023-04-20 15:30:44 +0200

    main now ready

--- src/main/java/minesweeper/Minesweeper.java
+++ src/main/java/minesweeper/Minesweeper.java
@@ -1,10 +1,35 @@
 package minesweeper;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Minesweeper {
+    private void printMinesweeperBoard(List<List<String>> board) {
+        for (List<String> row : board) {
+            System.out.println(row);
+        }
+    }
+
+    public String[][] generateMinesweeperBoard(String[][] board) {
+
+        return board;
+    }
 
     public List<List<String>> generateBoard(List<List<String>> board) {
+        System.out.println("\nboard state before generated board:");
+        printMinesweeperBoard(board);
+        /* Possible code */
+
+        for (int row = 0; row < board.size(); row++) {
+            for (int col = 0; col < board.get(row).size(); col++) {
+                /* Possible code */
+            }
+        }
+
+        /* Possible code */
+
+        // System.out.println("\nboard state after generated board:");
+        // printMinesweeperBoard(board);
 
         if(board.equals(Board.of("[*]"))) return board;
         return Board.of("[0]");

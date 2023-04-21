565a9f6058daf78145591391ab45204101ca515f
Alexander Fredheim <alfr@netlight.com>
2023-04-21 10:36:01 +0200

    shell start

--- src/main/java/minesweeper/Minesweeper.java
+++ src/main/java/minesweeper/Minesweeper.java
@@ -2,27 +2,6 @@ package minesweeper;
 
 public class Minesweeper {
 
-    public Board generateBoard(Board board) {
-        System.out.println("\nboard state before generated board:");
-        System.out.println(board);
-
-        /* Possible code */
-
-        for (int row = 0; row < board.getNumRows(); row++) {
-            for (int col = 0; col < board.getNumCols(); col++) {
-                if (board.getCell(row, col).equals("*")) continue;
-                board.updateCell(row, col, "0");
-            }
-        }
-
-        /* Possible code */
-
-        System.out.println("\nboard state after generated board:");
-        System.out.println(board);
-
-        return board;
-    }
-
     public static void main(String[] args) {
 
     }
--- src/test/java/minesweeper/MinesweeperTest.java
+++ src/test/java/minesweeper/MinesweeperTest.java
@@ -7,33 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MinesweeperTest {
     private final Minesweeper minesweeper = new Minesweeper();
 
-    // when input board [-], expect output board to be [0]
     @Test
-    void when_input__1x1_board_with_no_bomb__expect_board_with_0_as_output(){
-        Board expectedOutputBoard = new Board("[0]");
-        Board actualOutputBoard = minesweeper.generateBoard(new Board("[-]"));
-
-        assertEquals(expectedOutputBoard.content(), actualOutputBoard.content());
+    void when_adding_1_with_1__expect_2_to_be_the_sum() {
+        int expectedSum = 2;
+        int actualSum = 1 + 1;
+        assertEquals(expectedSum, actualSum);
     }
-
-    // when input board [*], expect output board to be [*]
-    @Test
-    void when_input__1x1_board_with_bomb__expect_board_with_bomb_as_output(){
-        Board expectedOutputBoard = new Board("[*]");
-        Board actualOutputBoard = minesweeper.generateBoard(new Board("[*]"));
-
-        assertEquals(expectedOutputBoard.content(), actualOutputBoard.content());
-    }
-
-    // when input board [* -], expect output board to be [* 1]
-    @Test
-    void when_input__1x2_board_with_first_as_bomb__expect_board_with_bomb_and_1_as_output(){
-        Board expectedOutputBoard = new Board("[* 1]");
-        Board actualOutputBoard = minesweeper.generateBoard(new Board("[* -]"));
-
-        assertEquals(expectedOutputBoard.content(), actualOutputBoard.content());
-    }
-
 }
 
 /*
